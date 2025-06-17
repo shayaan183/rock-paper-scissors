a8e31c028848a23b287bfa9c9f03805f096861e1
Shayaan <shayaan.musthafa2005@gmail.com>
2025-06-16 22:01:20 -0600

    Created the logic to play a single round

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);;
+    let computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice == 0) {
         return "rock";
@@ -11,5 +14,28 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Please enter your choice: rock, paper, or scissors")
+    return prompt("Please enter your choice: rock, paper, or scissors");
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == computerChoice) {
+        console.log("It's a tie!");
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") || 
+        (humanChoice == "paper" && computerChoice == "rock") || 
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+        humanScore++;
+    } else {
+        console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
+        computerScore++;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
